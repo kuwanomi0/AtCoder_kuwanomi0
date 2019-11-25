3da0a141f8d92b71e3f509d5f985bffada86ff77
kwatana20 <kwatana9102@gmail.com>
2019-11-25 16:48:35 +0900

    ABC086A 完了

---
+++ AtCoder_Beginners_Selection/ABC086A_Product/Main.py
@@ -0,0 +1,8 @@
+# ABC086A - Product
+# https://atcoder.jp/contests/abs/tasks/abc086_a
+# encode : utf-8
+a, b = map(int, input().split())
+if (a*b % 2 == 0) :
+    print('Even')
+else :
+    print('Odd')
---
+++ AtCoder_Beginners_Selection/PracticeA_Welcome_to_AtCoder/Main.py
@@ -0,0 +1,6 @@
+# http://atcoder.jp/contests/abs/tasks/practice_1
+# encode : utf-8
+a = int(input())
+b, c = map(int, input().split())
+s = input()
+print("{} {}".format(a + b + c, s))
