125321bbdc605b1eaae577e8ef5612014174cf02
kwatana9102 <kwatana9102@gmail.com>
2019-11-26 10:41:53 +0900

    ABC081B Complete

---
+++ AtCoder_Beginners_Selection/ABC081B_Shift_only/Main.py
@@ -0,0 +1,18 @@
+# AtCoder Beginners Selection
+# ABC081B - Shift only
+# https://atcoder.jp/contsts/abs/tasks/abc081_b
+# encode : utf-8
+count = 0
+find_odd = 0
+num = int(input())
+number_list = list(map(int, input().split()))
+
+while (1) :
+    odd_list = list(map(lambda x: int(x % 2), number_list))
+    find_odd = odd_list.count(1)
+    if (find_odd != 0) :
+        break
+    number_list = list(map(lambda x: int(x / 2), number_list))
+    count = count + 1
+
+print(count)
