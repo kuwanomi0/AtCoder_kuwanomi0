83aedccbcd9ea179cfe5011f3f49b5f44db22209
NSW00_009102 <kwatana20@gw.nsw.co.jp>
2019-11-26 16:34:59 +0900

    ABC088B Complete

--- AtCoder_Beginners_Selection/ABC088B_Card_ Game_for_Two/Main.py
+++ AtCoder_Beginners_Selection/ABC088B_Card_ Game_for_Two/Main.py
@@ -1,3 +1,7 @@
 # AtCoder Beginners Selection
 # ABC088B - Card Game for Two
 # https://atcoder.jp/contests/abs/tasks/abc088_b
+N = input()
+card_list = list(map(int, input().split()))
+card_list.sort(reverse=True)
+print(sum(card_list[0::2]) - sum(card_list[1::2]))
