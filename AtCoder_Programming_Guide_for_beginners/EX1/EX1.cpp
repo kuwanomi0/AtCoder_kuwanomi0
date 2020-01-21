9b6e761b4c79e84cccc4cec727b5520ad3b90eb8
NSW00_009102 <kwatana20@gw.nsw.co.jp>
2020-01-21 14:27:22 +0900

    C++入門 EX1まで

---
+++ AtCoder_Programming_Guide_for_beginners/EX1/EX1.cpp
@@ -0,0 +1,9 @@
+// EX1 - コードテストと出力の練習 / 1.01
+// https://atcoder.jp/contests/apg4b/tasks/APG4b_cv
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "こんにちは" << endl;
+    cout << "AtCoder" << endl;
+}
